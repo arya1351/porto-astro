09f813fcd5cfd24f5d606891d52812e2c9a4607e
ardut1351 <dagung34@gmail.com>
2025-03-23 10:46:24 +0700

    last banget ini mah

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,24 +1,22 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_BAemfyuk.mjs';
+import { manifest } from './manifest_Dh7XyKAQ.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
 
-const _page0 = () => import('./pages/_image.astro.mjs');
-const _page1 = () => import('./pages/certificate.astro.mjs');
-const _page2 = () => import('./pages/project/projectone.astro.mjs');
-const _page3 = () => import('./pages/project/projecttwo.astro.mjs');
-const _page4 = () => import('./pages/project.astro.mjs');
-const _page5 = () => import('./pages/index.astro.mjs');
+const _page0 = () => import('./pages/certificate.astro.mjs');
+const _page1 = () => import('./pages/project/projectone.astro.mjs');
+const _page2 = () => import('./pages/project/projecttwo.astro.mjs');
+const _page3 = () => import('./pages/project.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
-    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
-    ["src/pages/certificate.astro", _page1],
-    ["src/pages/project/projectone.astro", _page2],
-    ["src/pages/project/projecttwo.astro", _page3],
-    ["src/pages/project.astro", _page4],
-    ["src/pages/index.astro", _page5]
+    ["src/pages/certificate.astro", _page0],
+    ["src/pages/project/projectone.astro", _page1],
+    ["src/pages/project/projecttwo.astro", _page2],
+    ["src/pages/project.astro", _page3],
+    ["src/pages/index.astro", _page4]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -29,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "d596a48d-eed7-4683-a2fb-7568c9b8ae8e"
+    "middlewareSecret": "b19638b3-0685-4400-b7b3-6dfd62efa991"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
