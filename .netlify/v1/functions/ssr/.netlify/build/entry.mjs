ab4e1e5952d193af4ba8e65cb463c6bcc74c776b
ardut1351 <dagung34@gmail.com>
2025-03-23 10:33:50 +0700

    last

---
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -0,0 +1,41 @@
+import { renderers } from './renderers.mjs';
+import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_BAemfyuk.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/certificate.astro.mjs');
+const _page2 = () => import('./pages/project/projectone.astro.mjs');
+const _page3 = () => import('./pages/project/projecttwo.astro.mjs');
+const _page4 = () => import('./pages/project.astro.mjs');
+const _page5 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/certificate.astro", _page1],
+    ["src/pages/project/projectone.astro", _page2],
+    ["src/pages/project/projecttwo.astro", _page3],
+    ["src/pages/project.astro", _page4],
+    ["src/pages/index.astro", _page5]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "d596a48d-eed7-4683-a2fb-7568c9b8ae8e"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
